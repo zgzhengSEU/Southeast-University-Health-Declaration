bcd146881b8ce7812047ed7012de576c141f2c5b
Zhigang Zheng <62554593+zgzhengSEU@users.noreply.github.com>
2021-04-29 14:30:53 +0800

    Update Tui.py

--- Tui.py
+++ Tui.py
@@ -7,5 +7,5 @@ def server_post(Subject, Message, Sckey):
     r = requests.post(url, data=d)
 
 def kutui_post(Subject, Message, Kutui_key):
-    url = 'https://push.xuthus.cc/send/' + Kutui_key + '?c=' + Subject + '\n\n' + Message
+    url = 'https://push.xuthus.cc/group/' + Kutui_key + '?c=' + Subject + '\n\n' + Message
     r = requests.post(url)
